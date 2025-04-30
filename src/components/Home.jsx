54f631e3d9d6089e8d4862df7a30b83a5051963a
Santiago <dellasala1992@gmail.com>
2025-04-30 14:01:43 -0300

    Arreglo del home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
+import "../styles/Home.css"; // Asegúrate de tener un archivo de estilos
 
 const Home = () => {
   return (
-    <div>
-      <h1>Bienvenido a la Tienda de Cartas de Yu-Gi-Oh!</h1>
-      <p>Explora nuestras cartas y compra las que más te gusten.</p>
+    <div className="home-container">
+      <div className="overlay">
+        <div className="text-box">
+          <h1 className="display-4">Bienvenido a la Tienda de Cartas de Yu-Gi-Oh!</h1>
+          <p className="lead">
+            Explora nuestras cartas y compra las que más te gusten.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
